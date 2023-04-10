b12b4f8066fbea5be7b79ba1f7cebd8fc2790779
ayushpatel2004 <ayushpatel140604@gmail.com>
2023-04-10 18:19:25 +0530

    Added redirection

--- admin/src/container/Form1.js
+++ admin/src/container/Form1.js
@@ -8,42 +8,42 @@ const Form1=()=>{
         <form>
             <label>
                 Team1:
-                <input type="text" name="team1" />
+                <input required type="text" name="team1" />
             </label>
             <br></br>
             <label>
                 Team2:
-                <input type="text" name="team2" />
+                <input required type="text" name="team2" />
             </label>
             <br></br>
             <label>
                 Team1 Player1:
-                <input type="text" name="team1_player1" />
+                <input required type="text" name="team1_player1" />
             </label>
             <br></br>
             <label>
                 Team1 Player2:
-                <input type="text" name="team1_player2" />
+                <input required type="text" name="team1_player2" />
             </label>
             <br></br>
             <label>
                 Team1 Player3:
-                <input type="text" name="team1_player3" />
+                <input required type="text" name="team1_player3" />
             </label>
             <br></br>
             <label>
                 Team1 Player4:
-                <input type="text" name="team1_player4" />
+                <input required type="text" name="team1_player4" />
             </label>
             <br></br>
             <label>
                 Team1 Player5:
-                <input type="text" name="team1_player5" />
+                <input required type="text" name="team1_player5" />
             </label>
             <br></br>
             <label>
                 Team1 Player6:
-                <input type="text" name="team1_player6" />
+                <input required type="text" name="team1_player6" />
             </label>
             <br></br>
             <label>
@@ -53,37 +53,37 @@ const Form1=()=>{
             <br></br>
             <label>
                 Team1 Player8:
-                <input type="text" name="team1_player8" />
+                <input required type="text" name="team1_player8" />
             </label>
             <br></br>
             <label>
                 Team1 Player9:
-                <input type="text" name="team1_player9" />
+                <input required type="text" name="team1_player9" />
             </label>
             <br></br>
             <label>
                 Team1 Player10:
-                <input type="text" name="team1_player10" />
+                <input required type="text" name="team1_player10" />
             </label>
             <br></br>
             <label>
                 Team1 Player11:
-                <input type="text" name="team1_player11" />
+                <input required type="text" name="team1_player11" />
             </label>
             <br></br>
             <label>
                 Team2 Player1:
-                <input type="text" name="team2_player1" />
+                <input required type="text" name="team2_player1" />
             </label>
             <br></br>
             <label>
                 Team2 Player2:
-                <input type="text" name="team2_player2" />
+                <input required type="text" name="team2_player2" />
             </label>
             <br></br>
             <label>
                 Team2 Player3:
-                <input type="text" name="team2_player3" />
+                <input required type="text" name="team2_player3" />
             </label>
             <br></br>
             <label>
@@ -93,39 +93,40 @@ const Form1=()=>{
             <br></br>
             <label>
                 Team2 Player5:
-                <input type="text" name="team2_player5" />
+                <input required type="text" name="team2_player5" />
             </label>
             <br></br>
             <label>
                 Team2 Player6:
-                <input type="text" name="team2_player6" />
+                <input required type="text" name="team2_player6" />
             </label>
             <br></br>
             <label>
                 Team2 Player7:
-                <input type="text" name="team2_player7" />
+                <input required type="text" name="team2_player7" />
             </label>
             <br></br>
             <label>
                 Team2 Player8:
-                <input type="text" name="team2_player8" />
+                <input required type="text" name="team2_player8" />
             </label>
             <br></br>
             <label>
                 Team2 Player9:
-                <input type="text" name="team2_player9" />
+                <input required type="text" name="team2_player9" />
             </label>
             <br></br>
             <label>
                 Team2 Player10:
-                <input type="text" name="team2_player10" />
+                <input required type="text" name="team2_player10" />
             </label>
             <br></br>
             <label>
                 Team2 Player11:
-                <input type="text" name="team2_player11" />
+                <input required type="text" name="team2_player11" />
             </label>
-            <input type="submit" value="Submit" />
+            <br></br>
+            <input onClick={() => navigate(-1)} type="submit" value="Submit" />
         </form>
     )
 }
--- admin/src/container/Result.js
+++ admin/src/container/Result.js
@@ -2,33 +2,33 @@ import { Button } from "@mui/material";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-const Form1=()=>{
+const Result=()=>{
     const navigate = useNavigate();
     return (
         <form>
             <label>
                 Winning Team:
-                <input type="text" name="team1" />
+                <input required type="text" name="winning_team" />
             </label>
             <br></br>
             <label>
                 Plaer1:
-                <input type="text" name="team2" />
+                <input required type="text" name="winning_player1" />
             </label>
             <br></br>
             <label>
                 Player2:
-                <input type="text" name="team1_player1" />
+                <input required type="text" name="winning_player2" />
             </label>
             <br></br>
             <label>
                 Player3:
-                <input type="text" name="team1_player2" />
+                <input required type="text" name="winning_player3" />
             </label>
             <br></br>
-            <input type="submit" value="Submit" />
+            <input onClick={() => navigate(-1)} type="submit" value="Submit" />
         </form>
     )
 }
 
-export default Form1
+export default Result
