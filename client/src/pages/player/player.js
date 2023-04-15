aae602a271764cb4fd13564f316a94a551f3df24
Shreya <s_shivkumar@cs.iitr.ac.in>
2023-04-16 02:07:10 +0530

    playerbet design modification

--- client/src/pages/player/player.js
+++ client/src/pages/player/player.js
@@ -6,6 +6,7 @@ import Loader from '../../components/Loader';
 
 const PlayerCard=(props) => {
     const navigate = useNavigate();
+    // console.log(props);
     const {setselectedplayer} = useContext(WrapperContext);
     const onsubmit = (e,player) => {
         setselectedplayer(player);
@@ -13,7 +14,7 @@ const PlayerCard=(props) => {
     }
     return(<>
         <button onClick={(e)=>onsubmit(e,props.playername)} type="button" class="btn btn-outline-secondary">
-            <img src="https://w7.pngwing.com/pngs/981/645/png-transparent-default-profile-united-states-computer-icons-desktop-free-high-quality-person-icon-miscellaneous-silhouette-symbol.png" alt="" />
+            <img className="image" src="https://w7.pngwing.com/pngs/981/645/png-transparent-default-profile-united-states-computer-icons-desktop-free-high-quality-person-icon-miscellaneous-silhouette-symbol.png" alt="" />
             {props.playername}
             </button>
         </>);
@@ -21,19 +22,17 @@ const PlayerCard=(props) => {
 
 const Player = () => {
 
-    const {playerlist1,playerlist2,selectedmatchdetails,loadingplayerdisplay} = useContext(WrapperContext);
+    const {playerlist1,playerlist2,selectedmatchdetails} = useContext(WrapperContext);
   return (
-    <div>{
-        loadingplayerdisplay?<Loader/>:
-        <div>
+    <>
     <div className="playerbet gradient__text">PLAYER BET</div>
     <div className='bet'>
     <div className='list'>
     <h1 className="gradient__text">{selectedmatchdetails.team1}</h1>
     <div className='box'>
          {playerlist1.map((player)=>{
-             return <PlayerCard playername={player}/>
-            })}
+            return <PlayerCard playername={player}/>
+         })}
     </div>
     </div>
     <div className='vs'>V/S</div>
@@ -42,13 +41,11 @@ const Player = () => {
     <div className='box'>
         {playerlist2.map((player)=>{
             return <PlayerCard playername={player}/>
-        })}
+         })}
     </div>
     </div>
     </div>
-        </div>
-}
-    </div>
+    </>
   )
 }
 
