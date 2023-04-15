80b10e59a561ac66330624f406053eaffcca9d9c
ayushpatel2004 <ayushpatel140604@gmail.com>
2023-04-16 01:55:15 +0530

    Change UI of team bet

--- client/src/pages/team/team.js
+++ client/src/pages/team/team.js
@@ -1,8 +1,10 @@
 import React,{ useContext } from 'react'
 import './team.css'
 import { useNavigate } from "react-router-dom";
-// import React, { useContext } from 'react'
-import { WrapperContext } from '../../context/wrappercontext'
+import { WrapperContext } from '../../context/wrappercontext';
+import { Navbar } from "../../containers";
+import team1 from '../../assets/team1.svg';
+import team2 from '../../assets/team2.svg';
 
 export default function TeamBet() {
 const navigate = useNavigate();
@@ -14,24 +16,23 @@ const onsubmit = (e,team)=>{
 }
   return (
     <>
-    <div className="playerbet gradient__text">TEAM BET</div>
-    <div className='bet'>
-    <div className='list'>
-    <button onClick={(e)=>onsubmit(e,selectedmatchdetails.team1)} type="button" class="btn btn-outline-secondary">
-            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/188px-Flag_of_India.svg.png" alt="" />
-            <h1 className="gradient__text">{selectedmatchdetails.team1}</h1>
-
-            </button>
-    </div>
-    <div className='vs'>V/S</div>
-    <div className='list'>
-    <button onClick={(e)=>onsubmit(e,selectedmatchdetails.team2)} type="button" class="btn btn-outline-secondary">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Flag_of_Pakistan.svg/188px-Flag_of_Pakistan.svg.png" alt="" />
-            <h1 className="gradient__text">{selectedmatchdetails.team2}</h1>
-
-            </button>
-    </div>
-    </div>
+      <Navbar/>
+      <div className="playerbet">TEAM BET</div>
+      <div className='bet'>
+        <div className='list'>
+          <button onClick={(e)=>onsubmit(e,selectedmatchdetails.team1)} type="button" class="btn btn-outline-secondary team1_button">
+            <img src={team1} alt="" />
+            <h1 className='team1'>{selectedmatchdetails.team1}</h1>
+          </button>
+        </div>
+        <div className='vs'>V/S</div>
+        <div className='list'>
+          <button onClick={(e)=>onsubmit(e,selectedmatchdetails.team2)} type="button" class="btn btn-outline-secondary team2_button">
+            <img src={team2} alt="" />
+            <h1 className='team2'>{selectedmatchdetails.team2}</h1>
+          </button>
+        </div>
+      </div>
     </>
   )
 }
