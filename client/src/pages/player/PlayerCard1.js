2ba6c7f716afb41ed2833ba086d0c24e41e0cab2
sanidhya2510 <sanidhya2510@gmail.com>
2023-04-16 16:38:15 +0530

    fixes player betting UI

---
+++ client/src/pages/player/PlayerCard1.js
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react'
+import { useNavigate } from "react-router-dom";
+import './playerCard.css';
+import { WrapperContext } from '../../context/wrappercontext';
+import { useState } from 'react';
+import Amount_Player from '../amount_player';
+import player1 from '../../assets/player1.svg';
+
+const PlayerCard1 = (props) => {
+    const navigate = useNavigate();
+    const {setselectedplayer} = useContext(WrapperContext);
+    const [ButtonState, setButtonState] = useState(false);
+    
+     onsubmit = (e,player) => {
+        setButtonState(true);
+        setselectedplayer(player);
+    }
+    return(
+        <>
+            <button onClick={(e)=>onsubmit(e,props.playername)} type="button" class="my-button team1_button">
+                <img src={player1} alt="My Image" className='player_image'/>
+                <h1 className='team1'>{props.playername}</h1>
+            </button>
+            <Amount_Player trigger = {ButtonState} setTrigger = {setButtonState}></Amount_Player>
+        </>
+        );
+}
+
+export default PlayerCard1
